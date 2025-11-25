2fcd0e1ddcc3cbb2f6c2845a5752247bca0009e8
AlmatariAhmed <almatariahmed22@gmail>
2025-11-24 20:49:45 -0800

    Add yemen-blog.onrender.com to ALLOWED_HOSTS

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-_-o5i+fbn!#(r2vi_dv!f@93i3qp)9$a)fg)9!5zbj1g*7a3kq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['yemen-blog.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
